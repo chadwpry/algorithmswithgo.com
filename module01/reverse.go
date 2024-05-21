84e49f9242087e9fddf571c3462db4c6267848d7
Chad W Pry <chad.pry@gmail.com>
2024-05-21 11:25:56 -0500

    Reverse with recursion

--- module01/reverse.go
+++ module01/reverse.go
@@ -1,11 +1,19 @@
 package module01
 
+import "unicode/utf8"
+
 // Reverse will return the provided word in reverse
 // order. Eg:
 //
-//   Reverse("cat") => "tac"
-//   Reverse("alphabet") => "tebahpla"
-//
+//	Reverse("cat") => "tac"
+//	Reverse("alphabet") => "tebahpla"
 func Reverse(word string) string {
-	return ""
+	if len(word) == 0 {
+		return ""
+	}
+
+	b := []byte(word)
+	r, w := utf8.DecodeRune(b)
+
+	return Reverse(word[w:]) + string(r)
 }
