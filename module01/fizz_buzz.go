ff8708f52b586fcc9bb5ba8a95918f5c3a13ed97
Chad W Pry <chad.pry@gmail.com>
2024-05-21 15:47:21 -0500

    add basic fizzbuzz

--- module01/fizz_buzz.go
+++ module01/fizz_buzz.go
@@ -1,5 +1,7 @@
 package module01
 
+import "fmt"
+
 // FizzBuzz will print out all of the numbers
 // from 1 to N replacing any divisible by 3
 // with "Fizz", and divisible by 5 with "Buzz",
@@ -11,4 +13,27 @@ package module01
 // I wouldn't normally recommend this, but did
 // it here to make life easier for beginners.
 func FizzBuzz(n int) {
+	output := []string{}
+
+	for i := 1; i <= n; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			output = append(output, "Fizz Buzz")
+		} else if i%3 == 0 {
+			output = append(output, "Fizz")
+		} else if i%5 == 0 {
+			output = append(output, "Buzz")
+		} else {
+			output = append(output, fmt.Sprint(i))
+		}
+	}
+
+	for j, val := range output {
+		fmt.Print(val)
+
+		if j == len(output)-1 {
+			fmt.Print("\n")
+		} else {
+			fmt.Print(", ")
+		}
+	}
 }
