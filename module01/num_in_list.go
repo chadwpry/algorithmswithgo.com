81547e32daa65f155ad192bc891e2b0234d8e751
Chad W Pry <chad.pry@gmail.com>
2024-05-21 11:07:19 -0500

    NumInList with recursion

--- module01/num_in_list.go
+++ module01/num_in_list.go
@@ -3,5 +3,13 @@ package module01
 // NumInList will return true if num is in the
 // list slice, and false otherwise.
 func NumInList(list []int, num int) bool {
-	return false
+	if len(list) == 0 {
+		return false
+	}
+
+	if list[0] == num {
+		return true
+	}
+
+	return NumInList(list[1:], num)
 }
