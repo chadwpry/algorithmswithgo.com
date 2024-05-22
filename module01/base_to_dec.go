a57d6e3a91c025988be63cf954e1bc0c8e4a515d
Chad W Pry <chad.pry@gmail.com>
2024-05-22 12:12:37 -0500

    BaseToDec with recursion

--- module01/base_to_dec.go
+++ module01/base_to_dec.go
@@ -1,13 +1,25 @@
 package module01
 
+import (
+	"math"
+)
+
 // BaseToDec takes in a number and the base it is currently
 // in and returns the decimal equivalent as an integer.
 //
 // Eg:
 //
-//   BaseToDec("E", 16) => 14
-//   BaseToDec("1110", 2) => 14
-//
+//	BaseToDec("E", 16) => 14
+//	BaseToDec("1110", 2) => 14
 func BaseToDec(value string, base int) int {
-	return 0
+	valueLen := len(value)
+
+	if valueLen == 0 {
+		return 0
+	}
+
+	powerBase := int(math.Pow(float64(base), float64(valueLen-1)))
+	columnValue := string(value[0])
+
+	return (powerBase * strToDecMap[columnValue]) + BaseToDec(value[1:], base)
 }
