0e8f92cede6ef2b2281c50e63540849f550b3318
Chad W Pry <chad.pry@gmail.com>
2024-05-22 18:51:30 -0500

    Factor with recursion
    - added one test case that uses a list with one number that is not a prime

--- module01/factor.go
+++ module01/factor.go
@@ -11,16 +11,31 @@ package module01
 //
 // Examples:
 //
-//   Factor([]int{2,3,5}, 30) // []int{2,3,5}
-//   Factor([]int{2,3,5}, 28) // []int{2,2,7}
-//   Factor([]int{2,3,5}, 720) // []int{2,2,2,2,3,3,5}
+//	Factor([]int{2,3,5}, 30) // []int{2,3,5}
+//	Factor([]int{2,3,5}, 28) // []int{2,2,7}
+//	Factor([]int{2,3,5}, 720) // []int{2,2,2,2,3,3,5}
 //
 // Examples with remainders:
 //
-//   Factor([2,5], 30) // []int{2,5,3}
-//   Factor([3,5], 720) // []int{3,3,5,16}
-//   Factor([], 4) // []int{4}
-//
+//	Factor([2,5], 30) // []int{2,5,3}
+//	Factor([3,5], 720) // []int{3,3,5,16}
+//	Factor([], 4) // []int{4}
+
 func Factor(primes []int, number int) []int {
-	return nil
+	if number == 1 {
+		return []int{}
+	}
+
+	if len(primes) == 0 {
+		return []int{number}
+	}
+
+	factors := []int{}
+
+	for number%primes[0] == 0 {
+		factors = append(factors, primes[0])
+		number = number / primes[0]
+	}
+
+	return append(factors, Factor(primes[1:], number)...)
 }
--- module01/factor_test.go
+++ module01/factor_test.go
@@ -16,13 +16,15 @@ func TestFactor(t *testing.T) {
 		{tenPrimes, 30, []int{2, 3, 5}},
 		{tenPrimes, 28, []int{2, 2, 7}},
 		{tenPrimes, 720, []int{2, 2, 2, 2, 3, 3, 5}},
-		{tenPrimes, 30, []int{2, 3, 5}},
-		{tenPrimes, 720, []int{2, 2, 2, 2, 3, 3, 5}},
-		{tenPrimes, 4, []int{2, 2}},
+		{[]int{2, 5, 7}, 30, []int{2, 5, 3}},
+		{[]int{3, 5, 7}, 720, []int{3, 3, 5, 16}},
+		{[]int{}, 4, []int{4}},
+		{[]int{2, 4, 7}, 30, []int{2, 15}}, // NON PRIME (4) TEST
 	}
 	for _, tc := range tests {
 		t.Run(fmt.Sprintf("%v with primes %v", tc.number, tc.primes), func(t *testing.T) {
 			got := Factor(tc.primes, tc.number)
+			fmt.Println(got)
 			sort.Ints(got)
 			sort.Ints(tc.want)
 			if err := intSlicesEqual(got, tc.want); err != nil {
