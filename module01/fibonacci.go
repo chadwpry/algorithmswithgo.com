25c58f383635cfd36599b4abb4c33d43b90c5491
Chad W Pry <chad.pry@gmail.com>
2024-05-22 19:09:54 -0500

    add caching to Fibonacci sequence

--- module01/fibonacci.go
+++ module01/fibonacci.go
@@ -22,10 +22,25 @@ package module01
 //	Fibonacci(6) => 8
 //	Fibonacci(7) => 13
 //	Fibonacci(14) => 377
+
+var cache = map[int]int{}
+
 func Fibonacci(n int) int {
 	if n == 0 || n == 1 {
 		return n
 	}
 
-	return Fibonacci(n-1) + Fibonacci(n-2)
+	fib1, ok := cache[n-1]
+	if ok == false {
+		cache[n-1] = Fibonacci(n - 1)
+		fib1 = cache[n-1]
+	}
+
+	fib2, ok := cache[n-2]
+	if ok == false {
+		cache[n-2] = Fibonacci(n - 2)
+		fib2 = cache[n-2]
+	}
+
+	return fib1 + fib2
 }
